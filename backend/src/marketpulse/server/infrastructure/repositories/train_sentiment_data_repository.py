97ae904aeb2530060fa7dbf46520ab9769847e75
Aditya Khadkikar <guskhadad@student.gu.se>
2024-01-05 18:28:45 +0100

    feat: model mods that right data is sent to training in admin-side

--- backend/src/marketpulse/server/infrastructure/repositories/train_sentiment_data_repository.py
+++ backend/src/marketpulse/server/infrastructure/repositories/train_sentiment_data_repository.py
@@ -1,4 +1,5 @@
 from ...models import TrainSentimentData
+import datetime
 
 class TrainSentimentDataRepository:
     @classmethod
@@ -17,6 +18,13 @@ class TrainSentimentDataRepository:
     def getCleanRows(row : str):
         try:
             return TrainSentimentData.objects.values_list('clean', 'sentiment')
+        except TrainSentimentData.DoesNotExist:
+            return None
+
+    @classmethod
+    def getCurrentRows(row : str):
+        try:
+            return TrainSentimentData.objects.filter(created_at__gte=(datetime.datetime.today()-datetime.timedelta(days=2)), created_at__lte=datetime.datetime.now()).values_list('clean', 'sentiment')
         except TrainSentimentData.DoesNotExist:
             return None
 
